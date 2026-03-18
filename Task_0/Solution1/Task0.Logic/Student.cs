0a5ab317c6cfc00bb89d97ca605b2321d71e18f9
Szymon Machała <257281@edu.p.lodz.pl>
2026-03-18 19:27:59 +0100

    Initial commit: Sample code with working NUnit tests

---
+++ Task_0/Solution1/Task0.Logic/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Task0.Logic
+{
+    public class Student(int id, string firstname, string lastname )
+    {
+        public int ID { get; set; } = id;
+        public string FirstName { get; set; } = firstname;
+        public string LastName { get; set; } = lastname;
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ Task_0/Solution1/Task0.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Task0.Logic;
+using NUnit.Framework;
+namespace Task0.Test;
+public class Tests
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        [Test]
+        public void TestStudentStoredProperties()
+        {
+            var s1 = new Student(2323, "Maciek", "Kubek");
+            var s2 = new Student(997, "Kuba", "Hubba Bubba");
+
+            Assert.That(s1.FirstName, Is.EqualTo("Maciek"));
+            Assert.That(s1.LastName, Is.EqualTo("Kubek"));
+            Assert.That(s1.ID, Is.EqualTo(2323));
+
+            Assert.That(s1.FirstName, Is.Not.EqualTo(s2.FirstName));
+            Assert.That(s1.LastName, Is.Not.EqualTo(s2.FirstName));
+            Assert.That(s1.ID, Is.Not.EqualTo(s2.ID));
+        }
+
+        [Test]
+        public void GetFullNameReturnsCorrectFullName()
+        {
+            var student = new Student(67, "Mariusz", "Pudzianowski");
+            var fullName = student.GetFullName();
+
+            Assert.That(fullName, Is.EqualTo("Mariusz Pudzianowski"));
+        }
+        
+
+    }
+}
